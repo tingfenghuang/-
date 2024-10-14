dc66cc918174ee51138ec74bbf26fea38a3bcc31
htingf <2956750166@qq.com>
2024-10-14 16:37:05 +0800

    路由滚动行为的优化

--- src/route/index.js
+++ src/route/index.js
@@ -28,6 +28,11 @@ const router = createRouter({
             path: '/login',
             component: Login,
         }
-    ]
+    ],
+    scrollBehavior() {
+        return {
+            top: 0
+        }
+    }
 })
 export default router
