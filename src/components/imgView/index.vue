9ab514e16335f5372eb57ea1ebc20b199e523f18
htingf <2956750166@qq.com>
2024-10-15 12:24:25 +0800

    滑块跟随鼠标

--- src/components/imgView/index.vue
+++ src/components/imgView/index.vue
@@ -1,10 +1,11 @@
 <template>
+      <!-- {{ elementX }},{{ elementY }},{{ isOutside}} -->
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -23,7 +24,8 @@
   </div>
 </template>
 <script setup>
-import {ref } from "vue"
+import {ref ,watch} from "vue"
+import { useMouseInElement } from "@vueuse/core"
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -33,6 +35,35 @@ const imageList = [
   "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
 ]
 const activeIndex=ref(0)
+const target = ref(null)
+const left=ref(0)
+const top = ref(0)
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+watch([elementX, elementY, isOutside], (newVal)=>{
+if(!isOutside.value){
+    if(elementX.value>100&&elementX.value<300){
+    left.value=elementX.value-100
+  }
+  if(elementY.value>100&&elementY.value<300){
+    top.value=elementY.value-100
+  }
+  if(elementX.value>300){
+    left.value=200
+  }
+  if(elementY.value>300){
+    top.value=200
+  }
+  if(elementX.value<100){
+    left.value=0
+  }
+  if(elementY.value<100){
+    top.value=0
+  }
+
+}
+
+ 
+})
 </script>
 
 <style scoped lang="scss">
